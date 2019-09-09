bcdcaf4086045723a02b2b605b8a309dd5ab6a9c
李子豪 <15836784857@163.com>
2019-09-09 13:52:32 +0800

    vue组件

---
+++ vue组件/LBevelAngle.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="bevel-angle-wrap">
+        <div class="bevel-angle">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+<!-- 斜角 -->
+<script>
+
+    export default {
+        name: "LBevelAngle",
+        props: {
+            type: {
+                default: ''
+            }
+        },
+        components: {},
+        data() {
+            return {}
+        },
+        computed: {},
+        mounted() {
+        },
+        methods: {}
+    }
+</script>
+
+<style scoped lang="less">
+    .bevel-angle-wrap {
+        position: absolute;
+        top: 0;
+        right: 0;
+        transform: rotate(45deg);
+
+        .bevel-angle {
+            position: absolute;
+            top: 15px;
+            left: 50%;
+            padding: 4px 0;
+            min-width: 100px;
+            transform: translateX(-50%);
+
+            font-size: 12px;
+            line-height: 1;
+            color: #ffffff;
+            text-align: center;
+
+            background-color: #f2581c;
+        }
+    }
+</style>
---
+++ vue组件/LRowScroll.vue
@@ -0,0 +1,41 @@
+<template>
+    <scroller lock-y :scrollbar-x="false">
+        <flexbox class="scroll_box" :style="{width:scrollWidth}">
+            <slot></slot>
+        </flexbox>
+    </scroller>
+</template>
+
+<script>
+    import {Scroller, Flexbox} from 'vux'
+
+    export default {
+        props: [
+            "list",
+            "itemWidth",
+        ],
+        components: {
+            Scroller,
+            Flexbox
+        },
+        computed: {
+            scrollWidth() {
+                let itemWidth = parseInt(this.itemWidth);
+                let unit = this.itemWidth.replace(/[0-9]/g, "");
+                if (this.list) {
+                    return this.list.length * itemWidth + unit;
+                } else {
+                    return "1000vw";
+                }
+            }
+        },
+    }
+</script>
+
+<style scoped lang="less">
+    .scroll_box {
+        flex-wrap: nowrap;
+        /*position: relative;*/
+        width: 1000vw;
+    }
+</style>
